f20d0f2a4d9c09fc1e34499e123824bcee2a45c2
runkuamr <arunbiomedical05@gmail.com>
2023-01-23 22:24:11 +0530

    add basic program

---
+++ starfor.py/listconstructure.py
@@ -0,0 +1,12 @@
+print(list(range(10)))
+a=[10,23,45,33,21,64,7,6,57,34,90,34,64,10]
+a.sort()
+print(a)
+a.sort(reverse=True)
+print(a)
+a.sort(key=len)
+print(a) 
+b=["hdhdndhn",'fjdhfuhjd','bhjfbwnfef','dfgibfqwf']
+print(b)
+b.sort(key=len)
+print(b)
